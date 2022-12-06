c9fe0d029a45bb05b713e31994caabbd40d09054
LucileSD <4316@holbertonschool.com>
2022-12-06 11:05:34 +0100

    locker

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""determine if all the boxes can be opened"""
+
+
+def canUnlockAll(boxes):
+    """function if we can open all boxes
+        boxes: a list of lists
+        Return True if all boxes can be opened, else return False
+    """
+    unlocked = [0]
+    for box_id, box in enumerate(boxes):
+        if not box:
+            continue
+        for key in box:
+            if key < len(boxes) and key not in unlocked and key != box_id:
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
+        return True
+    return False

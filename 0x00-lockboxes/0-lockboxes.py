e499e84113dc3f3c5030c6920de03b4ca0367e33
LucileSD <4316@holbertonschool.com>
2022-12-06 12:08:58 +0100

    to try

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -8,11 +8,11 @@ def canUnlockAll(boxes):
         Return True if all boxes can be opened, else return False
     """
     unlocked = [0]
-    for box_id, box in enumerate(boxes):
+    for box_idx, box in enumerate(boxes):
         if not box:
             continue
         for key in box:
-            if key < len(boxes) and key not in unlocked and key != box_id:
+            if key < len(boxes) and key not in unlocked and key != box_idx:
                 unlocked.append(key)
     if len(unlocked) == len(boxes):
         return True

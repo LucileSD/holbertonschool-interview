f4b00a2c41d52bb9b76a83285cec05b12d9734a9
LucileSD <4316@holbertonschool.com>
2022-12-06 14:02:14 +0100

    shorter

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -9,11 +9,7 @@ def canUnlockAll(boxes):
     """
     unlocked = [0]
     for box_idx, box in enumerate(boxes):
-        if not box:
-            continue
         for key in box:
             if key < len(boxes) and key not in unlocked and key != box_idx:
                 unlocked.append(key)
-    if len(unlocked) == len(boxes):
-        return True
-    return False
+    return len(unlocked) == len(boxes)

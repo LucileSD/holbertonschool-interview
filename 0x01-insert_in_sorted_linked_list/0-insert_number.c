0c56fa32d79dc2f5de88f9c1d29df2af662596c4
LucileSD <4316@holbertonschool.com>
2022-12-12 15:21:52 +0100

    betty

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -5,37 +5,39 @@
 
 /**
 * insert_node - insert a node with a value to the correct place in the list
-* head: the head of the list
-* number: the number to insert in the node
+* @head: the head of the list
+* @number: the number to insert in the node
 * Return: the address of the new node, or NULL if it failed
 */
-listint_t *insert_node(listint_t **head, int number) {
-
+listint_t *insert_node(listint_t **head, int number)
+{
 	listint_t *new_node, *tmp = *head;
 
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
 		return (NULL);
-	
+
 	new_node->n = number;
 	new_node->next = NULL;
 
-	while(tmp->next && tmp->next->n < number){
+	while (tmp->next && tmp->next->n < number)
 		tmp = tmp->next;
-	}
 
-	if(tmp == *head){
+	if (tmp == *head)
+	{
 		new_node->next = *head;
 		*head = new_node;
 	}
-	else if(tmp->next == NULL){
+	else if (tmp->next == NULL)
+	{
 		new_node->next = NULL;
 		tmp->next = new_node;
 	}
-	else{
+	else
+	{
 		new_node->next = tmp->next;
 		tmp->next = new_node;
 	}
 
-	return(new_node);
+	return (new_node);
 }

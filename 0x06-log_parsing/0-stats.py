19c1d51cc45266e91df5f9bebea94d5b7d0de71b
LucileSD <4316@holbertonschool.com>
2023-01-25 12:13:42 +0100

    fix

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -5,49 +5,49 @@
 from sys import stdin
 
 
-if __name__ == "__main__":
+lineCount = 0
+totalSize = 0
+statusCount = {
+    200: 0,
+    301: 0,
+    400: 0,
+    401: 0,
+    403: 0,
+    404: 0,
+    405: 0,
+    500: 0
+}
 
-    lineCount = 0
-    totalSize = 0
-    statusCount = {
-        200: 0,
-        301: 0,
-        400: 0,
-        401: 0,
-        403: 0,
-        404: 0,
-        405: 0,
-        500: 0
-    }
 
-    def print_stat(totalSize, statusCount):
-        """
-            print stats
-            args:
-                total_size
-                status_count
-        """
-        print(f"File size: {totalSize}")
-        for status in sorted(statusCount.keys()):
-            if statusCount[status] > 0:
-                print("{}: {}".format(status, statusCount[status]))
+def print_stat(totalSize, statusCount):
+    """
+        print stats
+        args:
+            total_size
+            status_count
+    """
+    print(f"File size: {totalSize}")
+    for status in sorted(statusCount.keys()):
+        if statusCount[status] > 0:
+            print("{}: {}".format(status, statusCount[status]))
 
-    try:
-        for line in stdin:
-            lineCount += 1
-            line = line.split()
-            status = int(line[7])
-            size = int(line[8])
-            totalSize += size
-            statusCount[status] += 1
-            if lineCount % 10 == 0:
-                print_stat(totalSize, statusCount)
+    if __name__ == "__main__":
+        try:
+            for line in stdin:
+                lineCount += 1
+                try:
+                    line = line.split()
+                    size = int(line[-1])
+                    totalSize += size
+                    status = int(line[-2])
+                    if status in statusCount.keys():
+                        statusCount[status] += 1
+                except ValueError:
+                    pass
+                if lineCount % 10 == 0:
+                    print_stat(totalSize, statusCount)
 
-    except ValueError:
-        pass
-
-    except KeyboardInterrupt:
-        print_stat(totalSize, statusCount)
-        raise
+        except KeyboardInterrupt:
+            print_stat(totalSize, statusCount)
 
     print_stat(totalSize, statusCount)

4a52d3dead82cdd6b8df696720c3756aec69bd85
LucileSD <4316@holbertonschool.com>
2023-02-13 10:34:24 +0100

    Convert iterable list to bytes

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -2,21 +2,14 @@
 """determines if a given data set represents a valid UTF-8 encoding"""
 
 
-from math import ceil, log
-
 def validUTF8(data):
     """a method that determines if a given data set represents
        a valid UTF-8 encoding
        args: data: data to check
        return: True if data is valid UTF-8 otherwise False
     """
-    list_utf_8 = []
-    for nb in data:
-        if nb == 0:
-            list_utf_8.append(0)
-        else:
-            list_utf_8.append(ceil(log(nb) / log(2)))
-    for bytes in list_utf_8:
-        if bytes > 7:
-            return False
+    try:
+        bytes(data)
+    except ValueError:
+        return False
     return True

2d6da6b62c8555f324df3b7d477add97c930b3d6
LucileSD <4316@holbertonschool.com>
2023-02-15 11:30:16 +0100

    try

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -8,8 +8,23 @@ def validUTF8(data):
        args: data: data to check
        return: True if data is valid UTF-8 otherwise False
     """
-    try:
-        bytes(data)
-    except ValueError:
-        return False
+    i = 0
+    while i < len(data):
+        if data[i] < 128:
+            n_bytes = 1
+        elif 192 <= data[i] < 224:
+            n_bytes = 2
+        elif 224 <= data[i] < 240:
+            n_bytes = 3
+        elif 240 <= data[i] < 248:
+            n_bytes = 4
+        else:
+            return False
+
+        for j in range(i + 1, i + n_bytes):
+            if j >= len(data) or data[j] < 128 or data[j] >= 192:
+                return False
+
+        i += n_bytes
+
     return True

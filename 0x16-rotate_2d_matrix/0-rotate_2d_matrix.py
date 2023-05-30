305663ffb1d0a53d289967ac53303fcaa4feff19
LucileSD <4316@holbertonschool.com>
2023-05-30 16:13:53 +0200

    rotate

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""module to rotate"""
+
+def rotate_2d_matrix(matrix):
+    """rotate a 2D matrix at 90 degrees clockwise"""
+    for row in range(len(matrix)):
+        for element in range(row, len(matrix)):
+            tmp = matrix[row][element]
+            matrix[row][element] = matrix[element][row]
+            matrix[element][row] = tmp
+
+    for row in matrix:
+        row.reverse()
